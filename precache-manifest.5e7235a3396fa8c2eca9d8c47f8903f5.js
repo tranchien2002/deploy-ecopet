06346b4361b2096252d9a9d2e08f11e2b37ae562
tran.minh.chien <FRAMGIA\tran.minh.chien@b121716-pc.framgia.com>
2019-07-17 16:37:19 +0700

    Updates

---
+++ precache-manifest.5e7235a3396fa8c2eca9d8c47f8903f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbdee45d5abfb4077856b94c8cf37de",
+    "url": "/deploy-ecopet/index.html"
+  },
+  {
+    "revision": "e0ab4117f67a070e7ff8",
+    "url": "/deploy-ecopet/static/css/main.c9fb46a6.chunk.css"
+  },
+  {
+    "revision": "44d0a6a2b7714098e5b7",
+    "url": "/deploy-ecopet/static/js/2.c17ea0b2.chunk.js"
+  },
+  {
+    "revision": "e0ab4117f67a070e7ff8",
+    "url": "/deploy-ecopet/static/js/main.6e9f7c63.chunk.js"
+  },
+  {
+    "revision": "2b2504b6a6561ed185a1",
+    "url": "/deploy-ecopet/static/js/runtime~main.125250d4.js"
+  }
+]);
